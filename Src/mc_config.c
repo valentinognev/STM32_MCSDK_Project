b4f3bf9e2fdfa1bddd16d1123bbc4f56e930e755
valentinognev <valento@rafael.co.il>
2023-03-04 12:05:59 +0200

    STO_PLL was added after mistakenly removed in the previous commit.

--- Src/mc_config.c
+++ Src/mc_config.c
@@ -133,6 +133,26 @@ SpeednTorqCtrl_Handle_t SpeednTorqCtrlM1 =
   .IdrefDefault =                    (int16_t)DEFAULT_FLUX_COMPONENT,
 };
 
+RevUpCtrl_Handle_t RevUpControlM1 =
+{
+  .hRUCFrequencyHz         = MEDIUM_FREQUENCY_TASK_RATE,
+  .hStartingMecAngle       = (int16_t)((int32_t)(STARTING_ANGLE_DEG)* 65536/360),
+  .bFirstAccelerationStage = (ENABLE_SL_ALGO_FROM_PHASE-1u),
+  .hMinStartUpValidSpeed   = OBS_MINIMUM_SPEED_UNIT,
+  .hMinStartUpFlySpeed     = (int16_t)(OBS_MINIMUM_SPEED_UNIT/2),
+  .OTFStartupEnabled       = false,
+  .OTFPhaseParams         = {(uint16_t)500,
+                                         0,
+                             (int16_t)PHASE5_FINAL_CURRENT,
+                             (void*)MC_NULL},
+  .ParamsData             = {{(uint16_t)PHASE1_DURATION,(int16_t)(PHASE1_FINAL_SPEED_UNIT),PHASE1_FINAL_CURRENT,&RevUpControlM1.ParamsData[1]},
+                             {(uint16_t)PHASE2_DURATION,(int16_t)(PHASE2_FINAL_SPEED_UNIT),PHASE2_FINAL_CURRENT,&RevUpControlM1.ParamsData[2]},
+                             {(uint16_t)PHASE3_DURATION,(int16_t)(PHASE3_FINAL_SPEED_UNIT),PHASE3_FINAL_CURRENT,&RevUpControlM1.ParamsData[3]},
+                             {(uint16_t)PHASE4_DURATION,(int16_t)(PHASE4_FINAL_SPEED_UNIT),PHASE4_FINAL_CURRENT,&RevUpControlM1.ParamsData[4]},
+                             {(uint16_t)PHASE5_DURATION,(int16_t)(PHASE5_FINAL_SPEED_UNIT),PHASE5_FINAL_CURRENT,(void*)MC_NULL},
+                            },
+};
+
 PWMC_R3_2_Handle_t PWM_Handle_M1=
 {
   {
@@ -209,6 +229,69 @@ VirtualSpeedSensor_Handle_t VirtualSpeedSensorM1 =
   .hSpeedSamplingFreqHz =	MEDIUM_FREQUENCY_TASK_RATE,
   .hTransitionSteps     =	(int16_t)(TF_REGULATION_RATE * TRANSITION_DURATION/ 1000.0),
 
+};
+
+/**
+  * @brief  SpeedNPosition sensor parameters Motor 1 - State Observer + PLL
+  */
+STO_PLL_Handle_t STO_PLL_M1 =
+{
+  ._Super = {
+	.bElToMecRatio                     =	POLE_PAIR_NUM,
+    .SpeedUnit                         = SPEED_UNIT,
+    .hMaxReliableMecSpeedUnit          =	(uint16_t)(1.15*MAX_APPLICATION_SPEED_UNIT),
+    .hMinReliableMecSpeedUnit          =	(uint16_t)(MIN_APPLICATION_SPEED_UNIT),
+    .bMaximumSpeedErrorsNumber         =	M1_SS_MEAS_ERRORS_BEFORE_FAULTS,
+    .hMaxReliableMecAccelUnitP         =	65535,
+    .hMeasurementFrequency             =	TF_REGULATION_RATE_SCALED,
+    .DPPConvFactor                     =  DPP_CONV_FACTOR,
+  },
+ .hC1                         =	C1,
+ .hC2                         =	C2,
+ .hC3                         =	C3,
+ .hC4                         =	C4,
+ .hC5                         =	C5,
+ .hF1                         =	F1,
+ .hF2                         =	F2,
+ .PIRegulator = {
+     .hDefKpGain = PLL_KP_GAIN,
+     .hDefKiGain = PLL_KI_GAIN,
+	 .hDefKdGain = 0x0000U,
+     .hKpDivisor = PLL_KPDIV,
+     .hKiDivisor = PLL_KIDIV,
+	 .hKdDivisor = 0x0000U,
+     .wUpperIntegralLimit = INT32_MAX,
+     .wLowerIntegralLimit = -INT32_MAX,
+     .hUpperOutputLimit = INT16_MAX,
+     .hLowerOutputLimit = -INT16_MAX,
+     .hKpDivisorPOW2 = PLL_KPDIV_LOG,
+     .hKiDivisorPOW2 = PLL_KIDIV_LOG,
+     .hKdDivisorPOW2       = 0x0000U,
+   },
+ .SpeedBufferSizeUnit                =	STO_FIFO_DEPTH_UNIT,
+ .SpeedBufferSizeDpp                 =	STO_FIFO_DEPTH_DPP,
+ .VariancePercentage                 =	PERCENTAGE_FACTOR,
+ .SpeedValidationBand_H              =	SPEED_BAND_UPPER_LIMIT,
+ .SpeedValidationBand_L              =	SPEED_BAND_LOWER_LIMIT,
+ .MinStartUpValidSpeed               =	OBS_MINIMUM_SPEED_UNIT,
+ .StartUpConsistThreshold            =	NB_CONSECUTIVE_TESTS,
+ .Reliability_hysteresys             =	M1_SS_MEAS_ERRORS_BEFORE_FAULTS,
+ .BemfConsistencyCheck               =	BEMF_CONSISTENCY_TOL,
+ .BemfConsistencyGain                =	BEMF_CONSISTENCY_GAIN,
+ .MaxAppPositiveMecSpeedUnit         =	(uint16_t)(MAX_APPLICATION_SPEED_UNIT*1.15),
+ .F1LOG                              =	F1_LOG,
+ .F2LOG                              =	F2_LOG,
+ .SpeedBufferSizeDppLOG              =	STO_FIFO_DEPTH_DPP_LOG,
+ .hForcedDirection                   =  0x0000U
+};
+
+STO_Handle_t STO_M1 =
+{
+  ._Super                        = (SpeednPosFdbk_Handle_t*)&STO_PLL_M1, //cstat !MISRAC2012-Rule-11.3
+  .pFctForceConvergency1         = &STO_PLL_ForceConvergency1,
+  .pFctForceConvergency2         = &STO_PLL_ForceConvergency2,
+  .pFctStoOtfResetPLL            = &STO_OTF_ResetPLL,
+  .pFctSTO_SpeedReliabilityCheck = &STO_PLL_IsVarianceTight
 };
 
 /**
